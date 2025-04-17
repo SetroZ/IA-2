698548357753344bd42f320c9e3adead7455b15c
SetroZ <joemosabry@gmail.com>
2025-04-17 12:15:54 +0800

    refactored to arrays

--- src/main/java/Algorithms/ParticleSwarm.java
+++ src/main/java/Algorithms/ParticleSwarm.java
@@ -6,12 +6,19 @@ import java.util.Random;
 import Model.Employee;
 import Model.Task;
 import Utilities.Initialise;
+import Utilities.Subject;
 
-public class ParticleSwarm implements Algorithm {
+public class ParticleSwarm implements Algorithm, Subject {
+
+    class GBestData {
+        double gBest;
+        int[] gBestArr;
+    }
 
     private List<Employee> employees;
     private List<Task> tasks;
     int populationSize;
+
     int maxIterations;
 
     public ParticleSwarm(List<Task> tasks, List<Employee> employees,
@@ -26,38 +33,54 @@ public class ParticleSwarm implements Algorithm {
 
     public void run() {
 
-        Fitness[] gBest = new Fitness[tasks.size()];
         int[][] swarms = Initialise.getInitialPopulation(employees, tasks, populationSize);
-        ParticleData[][] partiData = new ParticleData[populationSize][tasks.size()];
+        double[][] v = new double[populationSize][tasks.size()]; // contains velocities for each position.
+        int[][] pBest = new int[populationSize][tasks.size()]; // contains pbest for each particle. which is an array of
+                                                               // the best positions for each position.
+        GBestData gBestData = new GBestData();// contains the best pBest found i.e gBest. which is an array of
+        // the best positions found.
+        gBestData.gBestArr = new int[populationSize];
+        double[] fitnessPBest = new double[populationSize]; // contains the fitness value for each pBest.
 
         // Intialize Velocities, positions, pBest and gBest
         for (int i = 0; i < populationSize; i++) {
             for (int j = 0; j < populationSize; j++) {
-                partiData[i][j].velocity = Math.random();
-                partiData[i][j].pBest.position = swarms[i][j];
-                partiData[i][j].pBest.fitness = swarms[i][j];
+                v[i][j] = Math.random();
+                pBest[i][j] = swarms[i][j];
+                fitnessPBest[i] = CostCalculator.calculateTotalCost(pBest[i], tasks, employees);
             }
         }
-        gBest = findGbest(partiData);
+        gBestData = findGbest(gBestData, fitnessPBest, pBest);
 
+        // Main loop.
         for (int n = 0; n < maxIterations; n++) {
-            for (int i = 0; i < partiData.length; i++) {
-                for (int j = 0; j < partiData[0].length; j++) {
-                    partiData[i][j].velocity = calculateVelocity(gBest[j].position, partiData[i][j], swarms[i][j]);
-                    swarms[i][j] = calculatePosition(partiData[i][j].velocity, swarms[i][j]);
+            if (gBestData.gBest == 0) {
+                return gBestData.gBestArr;
+            }
+            for (int i = 0; i < populationSize; i++) {
+                for (int j = 0; j < tasks.size(); j++) {
+                    v[i][j] = calculateVelocity(gBestData.gBestArr[j], pBest[i][j], v[i][j], swarms[i][j]);
+                    swarms[i][j] = calculatePosition(v[i][j], swarms[i][j]);
+                    // Find pBest
+                    double newCost = CostCalculator.calculateTotalCost(swarms[i], tasks, employees);
+                    if (newCost <= fitnessPBest[i]) {
+                        fitnessPBest[i] = newCost;
+                        pBest[i] = swarms[i];
+                    }
                 }
             }
+            gBestData = findGbest(gBestData, fitnessPBest, pBest);
         }
 
     }
 
-    private double calculateVelocity(double gBest, ParticleData currPar, int currP) {
+    private double calculateVelocity(double gBest, int pBest, double v, int currP) {
         double c1 = 1.5;
         double c2 = 1.5;
         double r1 = Math.random();
         double r2 = Math.random();
         double w = 0.5;
-        double newV = w * (currPar.velocity) + c1 * r1 * (currPar.pBest.position - currP) + c2 * r2 * (gBest - currP);
+        double newV = w * (v) + c1 * r1 * (pBest - currP) + c2 * r2 * (gBest - currP);
         return sigmoid(newV);
 
     }
@@ -77,26 +100,16 @@ public class ParticleSwarm implements Algorithm {
         return 1 / (1 + Math.pow(Math.E, -v));
     }
 
-    private Fitness[] findGbest(ParticleData[][] particleData) {
-        double gBest = Integer.MAX_VALUE;
-        Fitness[] gBestArr = new Fitness[tasks.size()];
-        for (int i = 0; i < particleData.length; i++) {
-
-            for (int j = 0; j < particleData[0].length; j++) {
-                gBest = Math.max(gBest, particleData[i][j].pBest.fitness);
+    private GBestData findGbest(GBestData currGBest, double[] fitnesspBest, int[][] pBest) {
+        GBestData newGBest = currGBest;
+        double gBest = currGBest.gBest;
+        for (int i = 0; i < populationSize; i++) {
+            if (gBest > fitnesspBest[i]) {
+                newGBest.gBest = fitnesspBest[i];
+                newGBest.gBestArr = pBest[i];
             }
-
         }
-        return gBestArr;
+
+        return newGBest;
     }
-}
-
-class ParticleData {
-    double velocity;
-    Fitness pBest;
-}
-
-class Fitness {
-    int position;
-    double fitness;
 }
